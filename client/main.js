a1327408cad2b3a863c171ba0b8e594157474310
aurbanski <aurbanski27@gmail.com>
2017-07-15 11:14:54 -0600

    Implemented add and delete

--- client/main.js
+++ client/main.js
@@ -9,3 +9,27 @@ Template.body.helpers({
     return Notes.find({});
   }
 });
+
+Template.add.events({
+  'submit .add-form': function() {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Notes.insert({text, createdAt: new Date() });
+
+    target.text.value = '';
+
+    $('#addModal').modal('close');
+
+    return false;
+  }
+});
+
+Template.note.events({
+  'click .delete-note': function() {
+    Notes.remove(this._id)
+    return false;
+  }
+});
